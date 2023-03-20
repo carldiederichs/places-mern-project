a9000a10d508e7503813c3ba779dfd42b776ce92
Carl Diederichs <carl-diederichs@t-online.de>
2023-03-20 22:28:48 +0100

    added places route with dummy data

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
-
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Redirect, 
+  Switch
+} from 'react-router-dom';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
@@ -13,6 +18,9 @@ const App = () => {
       <Switch>
         <Route path='/' exact>
           <Users />
+        </Route>
+        <Route path='/:userId/places' exact>
+          <UserPlaces />
         </Route>
         <Route path='/places/new' exact>
           <NewPlace />
