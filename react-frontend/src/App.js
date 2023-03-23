c654aef44bb3e6ec24894ff0f32b986ce8c1a7b3
Carl Diederichs <carl-diederichs@t-online.de>
2023-03-23 17:48:49 +0100

    added Login/Logout context

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { 
   BrowserRouter as Router, 
   Route, 
@@ -11,12 +11,23 @@ import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
 import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import { AuthContext } from './shared/context/auth-context';
 
 const App = () => {
-  return( 
-  <Router>
-    <MainNavigation />
-    <main>
+  const [isLoggedIn, setIsLoggedIn] = useState(false); 
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true); 
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false); 
+  }, []);
+
+  let routes; 
+
+  if (isLoggedIn) {
+    routes = (
       <Switch>
         <Route path='/' exact>
           <Users />
@@ -30,13 +41,37 @@ const App = () => {
         <Route path='/places/:placeId'>
           <UpdatePlace />
         </Route>
+        <Redirect to='/'/>
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path='/' exact>
+          <Users />
+        </Route>
+        <Route path='/:userId/places' exact>
+          <UserPlaces />
+        </Route>
         <Route path='/auth'>
           <Auth/> 
         </Route>
-        <Redirect to='/'/>
+        <Redirect to='/auth'/>
       </Switch>
-    </main>
-  </Router>
+    );
+  }
+
+  return( 
+  <AuthContext.Provider 
+    value={{isLoggedIn: isLoggedIn, login: login, logout: logout}}
+  >
+    <Router>
+      <MainNavigation />
+      <main>
+          {routes}
+      </main>
+    </Router>
+  </AuthContext.Provider>
   );
 };
 
