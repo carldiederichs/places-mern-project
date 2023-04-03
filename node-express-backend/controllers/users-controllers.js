9bbb8166d3cd51d3663f86545eb5814c2d926ec5
Carl Diederichs <carl-diederichs@t-online.de>
2023-04-03 23:23:50 +0200

    added option to attribute places to a user

--- node-express-backend/controllers/users-controllers.js
+++ node-express-backend/controllers/users-controllers.js
@@ -30,7 +30,7 @@ const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return next(
-      new HttpError('Invalid inputs passed, please check your dat.', 422)
+      new HttpError('Invalid inputs passed, please check your data.', 422)
     );
   }
   const { name, email, password } = req.body;
