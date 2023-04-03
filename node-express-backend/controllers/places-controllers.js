ba0857d01d2f9d539b049ffaa45d8861faa3826f
Carl Diederichs <carl-diederichs@t-online.de>
2023-04-03 23:32:04 +0200

    added option to delete places attributed to a user

--- node-express-backend/controllers/places-controllers.js
+++ node-express-backend/controllers/places-controllers.js
@@ -180,7 +180,7 @@ const deletePlace = async (req, res, next) => {
 
   let place; 
   try {
-    place = await Place.findById(placeId);
+    place = await Place.findById(placeId).populate('creator');
   } catch (err) { 
     const error = new HttpError(
       'Something went wrong, could not delete place.', 
@@ -188,9 +188,22 @@ const deletePlace = async (req, res, next) => {
     );
     return next(error);
   }
+  
+  if (!place) {
+    const error = new HttpError(
+      'Could not find place for this id', 
+      400
+    );
+    return next(error);
+  }
 
   try {
-    await place.deleteOne();
+    const sess = await mongoose.startSession(); 
+    sess.startTransaction();
+    await place.deleteOne({ session: sess });
+    place.creator.places.pull(place);
+    await place.creator.save({ session: sess });
+    await sess.commitTransaction();
   } catch (err) {
     const error = new HttpError(
       'Something went wrong, could not delete place.', 
