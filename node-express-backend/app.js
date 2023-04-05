878523d1cf270cfcba31716438591663212eadf1
Carl Diederichs <carl-diederichs@t-online.de>
2023-04-05 23:13:12 +0200

    added CORS error handling

--- node-express-backend/app.js
+++ node-express-backend/app.js
@@ -10,6 +10,16 @@ const app = express();
 
 app.use(bodyParser.json()); 
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader(
+    'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE'); 
+  
+  next();
+}); 
+
 app.use('/api/places', placesRoutes); 
 app.use('/api/users', usersRoutes); 
 
@@ -27,7 +37,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect('mongodb+srv://carl:j614fEFkOf9we97I@cluster0.lum5y2y.mongodb.net/places?retryWrites=true&w=majority')
+  .connect('mongodb+srv://carl:j614fEFkOf9we97I@cluster0.lum5y2y.mongodb.net/mern?retryWrites=true&w=majority')
   .then(() => {
     app.listen(5000);
   })
