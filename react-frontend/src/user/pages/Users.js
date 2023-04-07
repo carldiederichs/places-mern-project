1ca46ba5b55df115d998a6a8a1fa6db451840249
Carl Diederichs <carl-diederichs@t-online.de>
2023-04-07 23:52:20 +0200

    enabled users GET request

--- react-frontend/src/user/pages/Users.js
+++ react-frontend/src/user/pages/Users.js
@@ -1,17 +1,50 @@
-import React from 'react'; 
+import React, { useEffect, useState } from 'react'; 
+
 import UsersList from '../components/UsersList'
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
 const Users = () => {
-    const USERS = [
-        {
-            id: 'u1', 
-            name: 'Carl Diederichs', 
-            image: 'https://avatars.githubusercontent.com/u/52462273?v=4', 
-            places: 3
-        }
-    ];
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
+    const [loadedUsers, setLoadedUsers] = useState();
+    useEffect(()=> {
+        const sendRequest = async () => {
+            setIsLoading(true);
+            try {
+                const response = await fetch('http://localhost:5000/api/users');
     
-    return <UsersList items={USERS}/>;
+                const responseData = await response.json();
+
+                if (!response.ok) {
+                    throw new Error(responseData.message); 
+                }
+    
+                setLoadedUsers(responseData.users);
+                setIsLoading(false);
+            } catch (err) {
+                setError(err.message); 
+            }
+            setIsLoading(false);
+        };
+        sendRequest();
+    }, []);
+
+    const errorHandler = () => {
+        setError(null);
+    }; 
+    
+    return (
+        <React.Fragment>
+            <ErrorModal error={error} onClear={errorHandler} />
+            {isLoading && (
+                <div className="center">
+                    <LoadingSpinner />
+                </div>
+            )}
+            {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+        </React.Fragment>
+    );
 };
 
 export default Users;
