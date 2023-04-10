57799ff10c67f736ee805f18aec2238a4e3d611c
Carl Diederichs <carl-diederichs@t-online.de>
2023-04-10 11:05:37 +0200

    refactored users-controllers.js

--- node-express-backend/controllers/users-controllers.js
+++ node-express-backend/controllers/users-controllers.js
@@ -12,8 +12,9 @@ const getUsers = async (req, res, next) => {
       'Fetching users failed, please try again later.', 
       500
     );
+    return next(error);
   }
-  res.json({users: users.map(user => user.toObject({ getters: true }))}); 
+  res.json({ users: users.map(user => user.toObject({ getters: true })) }); 
 };
 
 const signup = async (req, res, next) => {
@@ -82,12 +83,16 @@ const login = async (req, res, next) => {
 
   if (!existingUser || existingUser.password !== password) {
     const error = new HttpError(
-      'Invalid credentials, could not login'
+      'Invalid credentials, could not login', 
+      401
     ); 
     return next(error); 
   }
 
-  res.json({message: 'Logged in!', user: existingUser.toObject({getters: true})});
+  res.json({
+    message: 'Logged in!',
+    user: existingUser.toObject({ getters: true })
+  });
 };
 
 exports.getUsers = getUsers; 
